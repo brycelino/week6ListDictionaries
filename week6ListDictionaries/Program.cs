4f91759ad1db69d324da4ac5e92ad86d21da99ac
brycelino <brycejeff16@gmail.com>
2022-04-13 16:38:10 -0500

    week6ListDictionaries
    When you post a message on Facebook, depending on the number of people who like your post, Facebook displays different information:
    
    -If no one likes your post, it doesn't display anything.
    -If only one person likes your post, it displays: [Friend's Name] likes your post.
    -If two people like your post, it displays: [Friend 1] and [Friend 2] like your post.
    -If more than two people like your post, it displays: [Friend 1], [Friend 2] and [Number of Other People] others like your post.

--- week6ListDictionaries/Program.cs
+++ week6ListDictionaries/Program.cs
@@ -1,47 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-/* If no one likes your post, it doesn't display anything.
-If only one person likes your post, it displays: [Friend's Name] likes your post.
-If two people like your post, it displays: [Friend 1] and [Friend 2] like your post.
-If more than two people like your post, it displays: [Friend 1], [Friend 2]
-and [Number of Other People] others like your post.
- * Write a program and continuously ask the user to enter different names
-until the user presses Enter (without supplying a name).
-Depending on the number of names provided, display a message based on the above pattern
-You should use a List when solving this one!
-    */
+using System.Collections.Generic; // Allows the use of lists and dictionaries 
+
 namespace week6ListDictionaries
 {
     class Program
     {
         static void Main(string[] args)
         {
-            problemOne();
-            
+            ProblemOne(); // calls method problem one and executes the code inside the method 
+            ProblemTwo();
         }
-        static public void problemOne() // Declares a method for first problem
+        static public void ProblemOne() // Declares a method for first problem
         {
-            string input;
-            List<string> list = new List<string>();
+            string input; // string input which will hold the users input 
+            List<string> list = new List<string>(); // Intitializes the list function allows us to call it 
 
+            while(true)
+            {
+                Console.WriteLine("Enter a name. Press enter with nothing typed to exit."); // asks the user to enter a name or enter on the keyboard to exit 
+                input = Console.ReadLine(); // Declares input string as the users input from console.readline 
+                if (input == "") //if the users leaves the writing space blank 
+                    return; //and presses enter on their keyboard they will exit
+                list.Add(input); // adds the users input from console.readline to the list 
+                
+
+                if (list.Count >= 3) // if user input has 3 + names it will display code below
+                    Console.WriteLine("{0}, {1}, {2} and others likes your post.", list[0], list[1], list.Count -2);
+                else if (list.Count == 2) // if user input has 2 names it will display code below
+                    Console.WriteLine("{0} and {1} likes your post.", list[0], list[1]);
+                else if (list.Count == 1) // if user input has 1 names it will display code below
+                    Console.WriteLine("{0} likes your post.", list[0]);
+        
+            } 
+           
+        }
+        /*Write a program that asks the user to enter a sentence. 
+         * Display each unique letter in the sentence, 
+         * and how many times each one appeared. 
+         * You should use a Dictionary when solving this one!
+         */
+        static public void ProblemTwo()
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>(); // Intializes the dictionary function declares it as sentence 
+            
+            string userInput; // declares string for variable that will hold the user input 
             do
             {
-                Console.WriteLine("Enter a name. When done press enter without any words to exit.");
-                input = Console.ReadLine();
-                if (input == "")
-                    return;
-                list.Add(input);
-              //  Console.WriteLine(input + " likes your post.");
+                Console.WriteLine("Enter a sentence. Press enter with nothing typed to exit. "); // asks the user to enter a sentence or ok to exit
+                userInput = Console.ReadLine(); //  obtains the users input and places it in the variable userinput 
+                userInput = userInput.Replace(" ", string.Empty); // blank spaces will no longer be apart of the users input.
 
-                if (list.Count > 3)
-                    Console.WriteLine( "{0}, {1} and {2} likes your post.", list[0], list[1],list.Count-1);
-                 //for(int i = 0; i < list.Count; i++)
-                 // {
-                 //    Console.WriteLine(list[i] + " likes your post.");
-                 //  }
-
-            } while (input != ""); 
-           
+                foreach (char i in userInput) // for each loop that determines how many char are in user input
+                {
+                    if (dict.ContainsKey(i)) // if statement that checks if the dictionary contains a specified char
+                    {
+                        dict[i]++; // increments a specific char by 1 everytime it is used in the users input 
+                    }
+                    else
+                    {
+                        dict[i] = 1; // if the char appears once it is stil displayed 
+                    }
+                }
+                    foreach(char i in userInput) //for each loop that determines how many char are in user input
+                    {
+                       if (dict[i] != 0 ) // if conditon that checks if a char is equal to 0
+                        {
+                            Console.WriteLine(i + "|" + dict[i].ToString()); // displays the single char on the left side and how many times it appears on the right side 
+                            dict[i] = 0; // sets dictionary char to 0 so letters that were used multiple times wont appear twice on the letter side
+                        }
+                    
+                    }
+                
+            } while (userInput != ""); // if the user enter all caps OK they will exit 
         }
     }
 }
